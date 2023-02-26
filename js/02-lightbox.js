6a6011e768e0e89409c9424b6bd89f2c2c193017
Patryk Lubke <lubke.patryk@gmail.com>
2023-02-26 20:50:55 +0100

    homework

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const galleryItem = createGalleryItems(galleryItems);
+
+function createGalleryItems(items) {
+  return items
+    .map(
+      (item) => `<a class="gallery__item" href=${item.original}>
+      <img class="gallery__image" src=${item.preview} alt=${item.description} />
+      </a>`
+    )
+    .join("");
+}
+
+gallery.innerHTML = galleryItem;
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionsDelay: 250,
+});
